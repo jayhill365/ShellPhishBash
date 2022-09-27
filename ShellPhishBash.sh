d4020bb1d5e5538d0f94223bbd3ead3ddfd7de72
Jay <19259608+jayhill365@users.noreply.github.com>
2022-09-27 16:03:34 -0700

    Create ShellPhishBash.sh

---
+++ ShellPhishBash.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get install git-all
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+sudo apt-get install unzip
+unzip ngrok-stable-linux-amd64.zip
+./ngrok http 5555
+git clone https://github.com/jayhill365/ShellPhish.git
+cd ShellPhish && chmod +x * && bash shellphish.sh
